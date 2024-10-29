bc0bccd8d7a8a0b9c906d70cccfee9961650de45
Khac Duy Le <duy.lekhac12@gmail.com>
2024-10-29 18:56:35 +0000

    Add an example

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Greet from "./components/Greet";
+import Person from "./components/Person";
+import Product from "./components/Product";
 
 const App = () => {
-  return <Greet/>
+  return (
+    <>
+      <Person name="Don" age={22}/>
+      <Product name="House" price={2600}/>
+    </>
+  )
 };
 
 export default App;
---
+++ src/components/Product.jsx
@@ -0,0 +1,11 @@
+
+const Product = (props) => {
+  return (
+    <div>
+        <h2>Name: {props.name}</h2>
+        <p>Price: {props.price}</p>
+    </div>
+  )
+}
+
+export default Product
